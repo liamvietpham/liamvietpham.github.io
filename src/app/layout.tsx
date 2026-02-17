6cfbbd2ee0107e6d2db8a158d59a4a838d40ab28
vietpham <pviet693@gmail.com>
2026-02-17 11:00:27 +0700

    feat: improve lighthouse score

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const poppins = Poppins({
     subsets: ["latin"],
     weight: ["400", "500", "600", "700"],
     display: "swap",
-    preload: false
+    preload: true
 });
 
 export const metadata: Metadata = {
